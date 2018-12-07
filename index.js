6f88e5357fd193f7dd19092eaebbb93e9420ff2b
Jason Goncalves <jgoncalves@protonmail.com>
2018-12-06 22:55:07 -0500

    initial files

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/** @format */
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// create a component
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    )
+};
 
-AppRegistry.registerComponent(appName, () => App);
+// render it to the device
