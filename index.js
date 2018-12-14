2f9c6d15fc4be9a1f26b18596d87ec42481edaf7
Jason Goncalves <jgoncalves@protonmail.com>
2018-12-13 22:21:00 -0500

    started building out Album Component

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
- import Header from './src/components/header';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create a component
 const App = () => (
-    <Header />
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 
